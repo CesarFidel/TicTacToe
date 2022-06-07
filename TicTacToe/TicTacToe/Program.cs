84c32273367aa9933f88bc13213819cb01752056
CesarFidel <cesar__fidel@hotmail.com>
2022-06-07 18:08:23 -0500

    Mejorado dibujo del tablero y Bug fix: no se marcaba bien el empate

--- TicTacToe/TicTacToe/Program.cs
+++ TicTacToe/TicTacToe/Program.cs
@@ -1,45 +1,48 @@
-﻿/* TO DO
- * Checar posición repetida
- * Mejorar dibujo del mapa
-*/
-
-
-
-
-// See https://aka.ms/new-console-template for more information
+﻿// See https://aka.ms/new-console-template for more information
 string[,] tablero = { { "1", "2", "3" }, { "4", "5", "6" }, { "7", "8", "9" } };
+string[,] tableroReinicio = { { "1", "2", "3" }, { "4", "5", "6" }, { "7", "8", "9" } };
 bool esTurnoCirculo = true;
-IniciaJuego();
+Juega();
 
-void IniciaJuego()
+void Juega()
 {
     bool hayGanador = false;
 
-    while (ChecaFinaldelJuego())
+    string jugar = "jugar";
+    while (jugar == "jugar")
     {
+        while (ChecaFinaldelJuego())
+        {
+            DibujaTablero();
+            TurnoJugador();
+
+            hayGanador = RevisaGanador(esTurnoCirculo);
+
+            if (hayGanador)
+                break;
+
+            esTurnoCirculo = !esTurnoCirculo;
+        }
+
         DibujaTablero();
-        TurnoJugador();
-
-        hayGanador = RevisaGanador(esTurnoCirculo);
-
         if (hayGanador)
-            break;
-
-        esTurnoCirculo = !esTurnoCirculo;
-    }
-
-    DibujaTablero();
-    if (hayGanador)
-    {
-        if (esTurnoCirculo)
-            Console.WriteLine("Ganó el jugador que va O");
+        {
+            if (esTurnoCirculo)
+                Console.WriteLine("Ganó el jugador que va O");
+            else
+                Console.WriteLine("Ganó el jugador que va X");
+        }
         else
-            Console.WriteLine("Ganó el jugador que va X");
-    }
-    else
-        Console.WriteLine("Hubo un empate");
+            Console.WriteLine("Hubo un empate");
 
+        Console.WriteLine("-------------------------------------------------------");
+        Console.WriteLine("Si quieres volver a jugar escribe 'jugar', sino presiona cualquier tecla y presione Enter");
+        jugar = Console.ReadLine();
+        if (jugar == "jugar")
+            tablero = tableroReinicio;
+    }
 }
+
 
 void TurnoJugador()
 {
@@ -57,7 +60,6 @@ void TurnoJugador()
         esEntreadaValida = VerificaEntradaValida(posicion);
     }
     while (esEntreadaValida == false);
-
 }
 
 bool VerificaEntradaValida(string? posicion)
@@ -72,6 +74,9 @@ bool VerificaEntradaValida(string? posicion)
             return false;
         else
         {
+            if (!ChecaPosicionOcupada(posicionTablero))
+                return false;
+
             CambiaSeñalTablero(posicionTablero);
             return true;
         }
@@ -79,6 +84,59 @@ bool VerificaEntradaValida(string? posicion)
     }
     else
         return false;
+}
+
+bool ChecaPosicionOcupada(int posicionTablero)
+{
+    switch (posicionTablero)
+    {
+        case 1:
+            if (tablero[0, 0] == "X" || tablero[0, 0] == "O")
+                return false;
+            else
+                return true;
+        case 2:
+            if (tablero[0, 1] == "X" || tablero[0, 1] == "O")
+                return false;
+            else
+                return true;
+        case 3:
+            if (tablero[0, 2] == "X" || tablero[0, 2] == "O")
+                return false;
+            else
+                return true;
+        case 4:
+            if (tablero[1, 0] == "X" || tablero[1, 0] == "O")
+                return false;
+            else
+                return true;
+        case 5:
+            if (tablero[1, 1] == "X" || tablero[1, 1] == "O")
+                return false;
+            else
+                return true;
+        case 6:
+            if (tablero[1, 2] == "X" || tablero[1, 2] == "O")
+                return false;
+            else
+                return true;
+        case 7:
+            if (tablero[2, 0] == "X" || tablero[2, 0] == "O")
+                return false;
+            else
+                return true;
+        case 8:
+            if (tablero[2, 1] == "X" || tablero[2, 1] == "O")
+                return false;
+            else
+                return true;
+        case 9:
+            if (tablero[2, 2] == "X" || tablero[2, 2] == "O")
+                return false;
+            else
+                return true;
+        default: return false;
+    }
 }
 
 void CambiaSeñalTablero(int posicionTablero)
@@ -145,72 +203,15 @@ void CambiaSeñalTablero(int posicionTablero)
 void DibujaTablero()
 {
     Console.Clear();
-    string marcadorTablero = "";
-    int lenght = tablero.Length;
-    for (int fila = 1; fila <= 9; fila++)
-    {
-        for (int columna = 1; columna <= 14; columna++)
-        {
-            if (ChecaPosicionJugar(fila, columna, out marcadorTablero))
-            {
-                Console.Write(marcadorTablero);
-            }
-            else if (columna % 5 == 0 && columna != 0 && columna + 1 != 15)
-            {
-                Console.Write("|");
-            }
-            else if (fila % 3 == 0 && fila != 0 && fila + 1 != 10)
-            {
-                Console.Write("_");
-            }
-            else
-                Console.Write(" ");
-        }
-        Console.WriteLine();
-    }
-}
-
-bool ChecaPosicionJugar(int i, int j, out string marcadorTablero)
-{
-    marcadorTablero = "-";
-
-    // Renglon 1
-    bool esPosicionRenglon11 = i == 2 && j == 2;
-    bool esPosicionRenglon12 = i == 2 && j == 7;
-    bool esPosicionRenglon13 = i == 2 && j == 12;
-    if (esPosicionRenglon11)
-        marcadorTablero = tablero[0, 0];
-    if (esPosicionRenglon12)
-        marcadorTablero = tablero[0, 1];
-    if (esPosicionRenglon13)
-        marcadorTablero = tablero[0, 2];
-    bool esPosicionPrimerRenglon = esPosicionRenglon11 || esPosicionRenglon12 || esPosicionRenglon13;
-
-    // Renglon 2
-    bool esPosicionRenglon21 = i == 5 && j == 2;
-    bool esPosicionRenglon22 = i == 5 && j == 7;
-    bool esPosicionRenglon23 = i == 5 && j == 12;
-    if (esPosicionRenglon21)
-        marcadorTablero = tablero[1, 0];
-    if (esPosicionRenglon22)
-        marcadorTablero = tablero[1, 1];
-    if (esPosicionRenglon23)
-        marcadorTablero = tablero[1, 2];
-    bool esPosicionSegundoRenglon = esPosicionRenglon21 || esPosicionRenglon22 || esPosicionRenglon23;
-
-    // Renglon 3
-    bool esPosicionRenglon31 = i == 8 && j == 2;
-    bool esPosicionRenglon32 = i == 8 && j == 7;
-    bool esPosicionRenglon33 = i == 8 && j == 12;
-    if (esPosicionRenglon31)
-        marcadorTablero = tablero[2, 0];
-    if (esPosicionRenglon32)
-        marcadorTablero = tablero[2, 1];
-    if (esPosicionRenglon33)
-        marcadorTablero = tablero[2, 2];
-    bool esPosicionTercerRenglon = esPosicionRenglon31 || esPosicionRenglon32 || esPosicionRenglon33;
-
-    return esPosicionPrimerRenglon || esPosicionSegundoRenglon || esPosicionTercerRenglon;
+    Console.WriteLine("    |    |    ");
+    Console.WriteLine(" {0}  | {1}  |  {2}  ", tablero[0, 0], tablero[0, 1], tablero[0, 2]);
+    Console.WriteLine("____|____|____");
+    Console.WriteLine("    |    |    ");
+    Console.WriteLine(" {0}  | {1}  |  {2}  ", tablero[1, 0], tablero[1, 1], tablero[1, 2]);
+    Console.WriteLine("____|____|____");
+    Console.WriteLine("    |    |    ");
+    Console.WriteLine(" {0}  | {1}  |  {2}  ", tablero[2, 0], tablero[2, 1], tablero[2, 2]);
+    Console.WriteLine("    |    |    ");
 }
 
 bool RevisaGanador(bool esTurnoCirculo)
@@ -288,16 +289,26 @@ bool RevisaGanadorCirculo()
 
 bool ChecaFinaldelJuego()
 {
-    int posocionesLibres = 0;
+    bool hayCasillasDisponibles;
+    int contadorCasillasDisponibles = 0;
     for (int i = 0; i < 3; i++)
     {
-        for (int j = 0; j < 2; j++)
+        for (int j = 0; j < 3; j++)
         {
-            if (posocionesLibres > 0)
-                return true;
-            if (tablero[i, j] != "-")
-                posocionesLibres++;
+            try
+            {
+                Int32.Parse(tablero[i, j]);
+                contadorCasillasDisponibles++;
+            }
+            catch
+            {
+
+            }
         }
     }
-    return false;
+
+    if (contadorCasillasDisponibles == 0)
+        return false;
+    else
+        return true;
 }
